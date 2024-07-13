8e0c62a8504fb2ecc4e6f3398cfe9dc5dae46882
Prateek Singh <prateeksinghk7@gmail.com>
2024-07-13 21:05:22 +0530

    added play, pause and fullScreen button. Made Toast to show speed and sound values.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const media=document.querySelector('#media');
 const fileopen=document.querySelector("#file");
 const videoDiv=document.querySelector(".video");
@@ -7,6 +6,12 @@ media.addEventListener("click",()=>{
     fileopen.click();
    
 })
+
+
+// Full Screen.......................
+
+
+
 
 fileopen.addEventListener("change",(obj)=>{
  
@@ -33,19 +38,163 @@ fileopen.addEventListener("change",(obj)=>{
     // video.controls=true; 
     videoDiv.appendChild(video);
     video.autoplay=true;
-    video.setAttribute("class","myvedio");
-    video.volume= 0;
+    video.volume=0.3
+    video.setAttribute("class","myvideo");
+
+    video.addEventListener("loadedmetadata",function(){
+        // your time will there
+    })
+
+    //make icons visible if video is there
+    fullScreen.style.cssText="opacity:1; pointer-events: auto;";
+    play.style.cssText="opacity:1; pointer-events: auto;";
+    pause.style.cssText="opacity:1; pointer-events: auto;";
+   
 });
+
+//Function to get video element
+const getVideo =()=>{
+return document.querySelector(".myvideo");
+}
+
 
 //********************* Volume UP *******************/
 
 
 const volumeUp=document.querySelector("#volumeUp");
-const volumeDown=document.querySelector("#volumeDown");
+
 volumeUp.addEventListener('click',()=>{
-    const video=document.querySelector(".myvedio");
-console.log(video);
-video.volume=video.volume + 0.25;`~`
+    const video=getVideo();
+if(video==null){
+    return;
+}
+
+if(video.volume > 0.95){
+    setToast("Max. Volume");
+    return;
+}
+
+video.volume = video.volume+0.025;
+
+setToast(Math.floor(video.volume * 100) + " %");
+})
+
+//********************* Volume Down *******************/
+
+
+
+const volumeDown=document.querySelector("#volumeDown");
+volumeDown.addEventListener('click',()=>{
+    const video=getVideo();
+if(video==null){
+    return;
+}
+
+if(video.volume < 0.1){
+    setToast("Min. Volume");
+    return;
+}
+
+video.volume = video.volume-0.025;
+
+setToast(Math.floor(video.volume * 100) + " %");
 })
 
 
+
+
+//************ pause the video
+const pause=document.querySelector('.fa-pause');
+pause.addEventListener("click",()=>{
+    const video=getVideo();
+    if(video==null){
+        return;
+    }
+    video.pause();
+    
+});
+
+
+//************** play the video
+const play=document.querySelector('.fa-play');
+play.addEventListener("click",()=>{
+    const video=getVideo();
+    if(video==null){
+        alert("please select a video first!!");
+        return;
+    }
+    video.play();
+
+
+    
+
+});
+
+
+//************ Speed Up
+
+const speedUp=document.querySelector("#speedUp");
+const speedDown=document.querySelector("#speedDown");
+
+speedUp.addEventListener("click",()=>{
+    const video=getVideo();
+    if(video==null){
+        return;
+    }
+    if(video.playbackRate > 2.75){
+        setToast("Max. Speed");
+        return;
+    }
+    video.playbackRate+= 0.25;
+ 
+    setToast(video.playbackRate + " X");
+
+})
+
+//*********** Speed Down
+
+speedDown.addEventListener("click",()=>{
+    const video=getVideo();
+    if(video==null){
+        return;
+    }
+    if(video.playbackRate < 0.26){
+        setToast("Min. Speed");
+        return;
+    }
+    video.playbackRate-= 0.25;
+
+    
+    setToast(video.playbackRate + " X");
+
+})
+
+
+// Toast.......................
+
+const toast=document.querySelector(".toast");
+function setToast(text){
+
+    toast.textContent=text;
+    toast.style.display="block";
+    console.log(text)
+    setTimeout(() => {
+        toast.style.display="none";
+    }, 3000);
+}
+
+    
+
+const fullScreen = document.querySelector(".fa-expand");
+
+fullScreen.addEventListener("click",()=>{
+   const video=getVideo();
+    
+    if(video==null){
+        // fullScreen.style.cssText="opacity:0.5; pointer-events: none;";
+        alert("please select a video first!!");
+        return;
+    }
+
+    video.requestFullscreen();
+})
